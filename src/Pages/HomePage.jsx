e8dfd0d216bd5a09421224c6e86a2906c834ec96
Arnavkesari <arnavkesari13@gmail.com>
2024-08-26 03:24:54 +0530

    Added Footer

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -3,6 +3,7 @@ import Navbar from '../Components/Navbar'
 import GameList from '../Components/GameList'
 import CategorySlider from '../Components/CategorySlider'
 import ReviewCard from '../Components/ReviewCard'
+import Footer from '../Components/Footer'
 const HomePage = () => {
   return (
     <>
@@ -10,6 +11,7 @@ const HomePage = () => {
     <GameList />
     <CategorySlider />
     <ReviewCard />
+    <Footer />
     </>
   )
 }
